f9d4a0d4ed9e7360e4d95e67836f1290831dc8e8
Jingjing Duan <jduan@twitter.com>
2014-09-07 23:07:39 -0700

    start chapter14

---
+++ chapter14/spawn_basic.ex
@@ -0,0 +1,23 @@
+defmodule SpawnBasic do
+  def greet do
+    receive do
+      {sender, msg} ->
+        send sender, {:ok, "Hello #{msg}"}
+        greet
+    end
+  end
+end
+
+# here's a client
+pid = spawn(SpawnBasic, :greet, [])
+
+send pid, {self, "World!"}
+receive do
+  {:ok, message} -> IO.puts message
+end
+
+send pid, {self, "Kermit!"}
+receive do
+  {:ok, message} -> IO.puts message
+  after 500 -> IO.puts "The greeter has gone away!"
+end
